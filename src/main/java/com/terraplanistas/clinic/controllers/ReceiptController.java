1b28ff4af85458877d16ca311dd828298d0435c9
Juan Castenos <00026223@uca.edu.sv>
2026-06-09 22:56:12 -0600

    :memo: Adding project structure

---
+++ src/main/java/com/terraplanistas/clinic/controllers/ReceiptController.java
@@ -0,0 +1,4 @@
+package com.terraplanistas.clinic.controllers;
+
+public class ReceiptController {
+}
---
+++ src/main/java/com/terraplanistas/clinic/services/LaboratoryService.java
@@ -0,0 +1,4 @@
+package com.terraplanistas.clinic.services;
+
+public interface LaboratoryService {
+}
