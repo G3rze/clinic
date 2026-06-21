de412c61687212a4a8da50ba970a7ed3f4feab54
Jorgon29 <00016823@uca.edu.sv>
2026-06-21 17:41:31 -0600

    feature: added flow of post appointment note
    Signed-off-by: Jorgon29 <00016823@uca.edu.sv>

--- src/main/java/com/terraplanistas/clinic/services/PostAppointmentNoteService.java
+++ src/main/java/com/terraplanistas/clinic/services/PostAppointmentNoteService.java
@@ -1,4 +1,12 @@
 package com.terraplanistas.clinic.services;
 
+import com.terraplanistas.clinic.domain.dto.request.PostAppointmentNoteRequest;
+import com.terraplanistas.clinic.domain.dto.response.PostAppointmentNoteResponse;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface PostAppointmentNoteService {
+    public PostAppointmentNoteResponse createNote(UUID doctor, PostAppointmentNoteRequest note);
+    public List<PostAppointmentNoteResponse> getNoteFromAppointment(UUID requester, UUID appointment);
 }
